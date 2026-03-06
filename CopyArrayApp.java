c775e148e1b856208aaed59005a0fb6cba930681
lucaclery1 <lc1188@exeter.ac.uk>
2026-03-06 12:03:10 +0000

    1.1 and started 1.2

---
+++ CopyArrayApp.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class CopyArrayApp{
+    public static void main(String[] args) {
+        int nVals = args.length;
+        // to create a new array of ints
+        int[] myVals = new int[nVals];
+        for (int i = 0; i < nVals; i++) {
+            int item = Integer.parseInt(args[i]);
+            // to allocate a value into the created array at index i
+            myVals[i] = item;
+        }
+        System.out.println("original values: " + Arrays.toString(myVals));
+    }
+}
